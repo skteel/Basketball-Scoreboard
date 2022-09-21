116cbb9f2b23fc78057c9897adcb5407126f7421
skteel <skt90a@acu.edu>
2022-09-21 14:10:45 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+let homeScoreBtnOne = document.getElementById("home-score-btn-1")
+let homeScoreEl = document.getElementById("home-score")
+let homeScore = 0
+
+function increaseHomeScoreOne(){
+    homeScore += 1
+    homeScoreEl.textContent = homeScore
+}
+let homeScoreBtnTwo = document.getElementById("home-score-btn-2")
+
+function increaseHomeScoreTwo(){
+    homeScore += 2
+    homeScoreEl.textContent = homeScore
+}
+function increaseHomeScoreThree(){
+    homeScore += 3
+    homeScoreEl.textContent = homeScore
+}
+let guestScoreBtnOne = document.getElementById("guest-score-btn-1")
+let guestScoreEl = document.getElementById("guest-score")
+let guestScore = 0
+
+function increaseGuestScoreOne(){
+    guestScore += 1
+    guestScoreEl.textContent = guestScore
+}
+let guestScoreBtnOTwo = document.getElementById("guest-score-btn-2")
+function increaseGuestScoreTwo(){
+    guestScore += 2
+    guestScoreEl.textContent = guestScore
+}
+let guestScoreBtnThree = document.getElementById("guest-score-btn-3")
+function increaseGuestScoreThree(){
+    guestScore += 3
+    guestScoreEl.textContent = guestScore
+}
